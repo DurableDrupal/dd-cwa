81098150d930ee276bd3dc65698c656738cdd464
Victor Kane <victorkane@gmail.com>
2017-08-30 10:12:02 -0300

    edits

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
       <v-card>
         <v-card-title class="headline">Bienvenidos al futuro hogar de la comunidad PraxisPoetica</v-card-title>
         <v-card-text>
-          <p>PraxisPoetica existirá para proveer recursos para la <a href="https://www.marxists.org/espanol/m-e/1840s/45-feuer.htm" title="Los filósofos no han hecho más que interpretar de diversos modo el mundo, pero de lo que se trata es de transformarlo." target="_blank">praxis</a> literaria.</p>
+          <p>PraxisPoetica existirá siempre para proveer recursos para la <a href="https://www.marxists.org/espanol/m-e/1840s/45-feuer.htm" title="Los filósofos no han hecho más que interpretar de diversos modo el mundo, pero de lo que se trata es de transformarlo." target="_blank">praxis</a> literaria.</p>
           <p>En el contexto de la comunidad de cultura abierta en lucha para refundar la sociedad sobre nuevas bases</p>
           <p>Sin la explotación del hombre por el hombre</p>
           <p>Aquí se econtrarán talleres, textos, maneras de participar individualmente y también formando grupos de afinidad para trabajar y disfrutar en la praxis poética.</p>
