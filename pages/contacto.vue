fbf43f3777c76ef61e048fffe4e016bea0220fd8
Victor Kane <victorkane@gmail.com>
2017-08-30 11:30:00 -0300

    Pagina Contacto inicial para #6

---
+++ pages/contacto.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <v-card>
+        <v-card-title class="headline">Comunicarse con PraxisPoetica</v-card-title>
+        <v-card-text>
+          <p>Comunicarse con nosotros a través de <v-btn fab small primary dark title="@praxispoetica" href="http://twitter.com/praxispoetica"><v-icon>fa-twitter</v-icon></v-btn></p>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
