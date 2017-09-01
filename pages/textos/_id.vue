9cbef22c8a17196d95054e6c19ade5034ec2c325
Victor Kane <victorkane@gmail.com>
2017-09-01 09:43:11 -0300

    Aplicar estilos adicionales a texto individual

--- pages/textos/_id.vue
+++ pages/textos/_id.vue
@@ -3,8 +3,12 @@
     <v-layout row wrap>
       <v-flex xs12>
         <v-card class="ma-1">
-          <v-card-title class="headline">{{textos.find(x => x.slug === slug).titulo}}</v-card-title>
-          <v-card-text><div v-html="textos.find(x => x.slug === slug).texto"></div></v-card-text>
+          <v-card-title class="headline mb-1">{{textos.find(x => x.slug === slug).titulo}}</v-card-title>
+          <v-card-text>
+            <p class="primary--text subheading mt-0">{{textos.find(x => x.slug === slug).autor}}</p>
+            <blockquote class="body-2" v-html="textos.find(x => x.slug === slug).texto"></blockquote>
+            <p class="primary--text mt-2">{{textos.find(x => x.slug === slug).fecha}}</p>
+          </v-card-text>
           <v-card-actions><p><nuxt-link to="/textos">Volver a la lista de textos</nuxt-link></p></v-card-actions>
         </v-card>
       </v-flex>
@@ -19,7 +23,6 @@
       'textos'
     ]),
     async asyncData ({ params }) {
-      console.log('params', params)
       return {
         slug: params.id
       }
