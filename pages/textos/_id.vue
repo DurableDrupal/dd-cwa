8cf57f869f71d7fd3e5a00a85b020af70b714e9e
Victor Kane <victorkane@gmail.com>
2017-09-01 08:16:01 -0300

    Aplicar estilos a card texto individual para #8

--- pages/textos/_id.vue
+++ pages/textos/_id.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="container">
-    <h2>{{textos.find(x => x.slug === slug).titulo}}</h2>
-    <div v-html="textos.find(x => x.slug === slug).texto"></div>
-    <p><nuxt-link to="/textos">Volver a la lista de textos</nuxt-link></p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-card class="ma-1">
+          <v-card-title class="headline">{{textos.find(x => x.slug === slug).titulo}}</v-card-title>
+          <v-card-text><div v-html="textos.find(x => x.slug === slug).texto"></div></v-card-text>
+          <v-card-actions><p><nuxt-link to="/textos">Volver a la lista de textos</nuxt-link></p></v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
