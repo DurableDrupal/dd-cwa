4038e93c577ca93b3f4f8d111ffd6acbf2448885
Victor Kane <victorkane@gmail.com>
2017-09-01 09:05:34 -0300

    Agregar estructura a los datos de textos, aplicar estilos adicionales en listado de textos para #8

--- pages/textos/index.vue
+++ pages/textos/index.vue
@@ -3,10 +3,17 @@
   <v-layout row wrap>
     <v-flex xs12 sm3 v-for="texto in textos" :key="texto.slug">
       <v-card class="ma-1">
-        <v-card-title class="headline">{{ texto.titulo }}</v-card-title>
-        <v-card-text v-html="texto.texto"></v-card-text>
+        <v-card-title class="headline texto-card">
+          <nuxt-link :to="{ name: 'textos-id', params: { id: texto.slug } }">
+            {{ texto.titulo }}
+          </nuxt-link>
+        </v-card-title>
+        <v-card-text>
+          <p>{{ texto.texto.replace(/<(?:.|\n)*?>/gm, '').substr(0,30) + '...' }}</p>
+        </v-card-text>
         <v-card-actions>
-          <nuxt-link :to="{ name: 'textos-id', params: { id: texto.slug } }">Ver {{ texto.titulo }} completo</nuxt-link>
+          <p>{{ texto.autor }}</p>
+          <p class="right">{{ texto.fecha }}</p>
         </v-card-actions>
       </v-card>
     </v-flex> 
@@ -22,3 +29,14 @@
     ])
   }
 </script>
+
+<style>
+.texto-card a {
+  text-decoration: none !important;
+  padding: 2px !important;
+}
+.texto-card a:hover {
+  color: #fff8e1 !important;
+  background-color: #c47754 !important;
+}
+</style>
--- store/index.js
+++ store/index.js
@@ -4,41 +4,57 @@ export const state = () => ({
     {
       titulo: 'Integro Semper',
       slug: 'integer-semper',
+      autor: 'Gangsta 3433434 343434343 43434334',
+      fecha: 'January 1, 1970',
       texto: '<p>Integer semper velit fo shizzle purus...</p>'
     },
     {
       titulo: 'Adipiscing Gansta',
       slug: 'adipiscing-gansta',
+      autor: 'Gangsta',
+      fecha: 'January 1, 1970',
       texto: '<p>Lorizzle ipsum shizznit sit owned, consectetuer adipiscing gangsta...</p>'
     },
     {
       titulo: 'Black Pellentesque',
       slug: 'black-pellentesque',
+      autor: 'Gangsta',
+      fecha: 'January 1, 1970',
       texto: '<p>Black pellentesque nibh et turpizzle. Stuff get down get down tortizzle...</p>'
     },
     {
       titulo: 'Sed Errizle',
       slug: 'sed-errizle',
+      autor: 'Gangsta',
+      fecha: 'January 1, 1970',
       texto: '<p><strong>Sed erizzle</strong>. Boofron izzle dolor fo shizzle turpis tempizzle crazy...</p>'
     },
     {
       titulo: 'Crunk Vizzle',
       slug: 'crunk-vizzle',
+      autor: 'Gangsta',
+      fecha: 'January 1, 1970',
       texto: '<p>Crunk vizzle, check out this. Pellentesque funky fresh tortor...</p>'
     },
     {
       titulo: 'Nullam Things',
       slug: 'nullam-things',
+      autor: 'Gangsta',
+      fecha: 'January 1, 1970',
       texto: '<p>Nullam things velit, pimpin\' volutpizzle, suscipizzle hizzle...</p>'
     },
     {
       titulo: 'Lorizzle Ipsum',
       slug: 'lorizzle-ipsum',
+      autor: 'Gangsta',
+      fecha: 'January 1, 1970',
       texto: '<p>Lorizzle ipsum shizznit sit owned, consectetuer adipiscing gangsta...</p>'
     },
     {
       titulo: 'That is the shizzle',
       slug: 'that-s-the-shizzle',
+      autor: 'Gangsta',
+      fecha: 'January 1, 1970',
       texto: '<p>That\'s the shizzle check it out.</p>'
     }
   ]
