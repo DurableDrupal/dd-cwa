37dd545d675fdd650983e7f0b5c94f677878bd0f
Victor Kane <victorkane@gmail.com>
2017-09-03 11:36:08 -0300

    Buscar datos sin async y sin repetitcion de filtro para #10

--- pages/textos/_id.vue
+++ pages/textos/_id.vue
@@ -3,11 +3,10 @@
     <v-layout row wrap>
       <v-flex xs12>
         <v-card class="ma-1">
-          <v-card-title class="headline mb-1">{{textos.find(x => x.slug === slug).titulo}}</v-card-title>
           <v-card-text>
-            <p class="primary--text subheading mt-0">{{textos.find(x => x.slug === slug).autor}}</p>
-            <blockquote class="body-1" v-html="textos.find(x => x.slug === slug).texto"></blockquote>
-            <p class="primary--text mt-2">{{textos.find(x => x.slug === slug).fecha}}</p>
+            <p class="primary--text subheading mt-0">{{texto.autor}}</p>
+            <blockquote class="body-1" v-html="texto.texto"></blockquote>
+            <p class="primary--text mt-2">{{texto.fecha}}</p>
           </v-card-text>
           <v-card-actions><p><nuxt-link to="/textos">Volver a la lista de textos</nuxt-link></p></v-card-actions>
         </v-card>
@@ -17,14 +16,10 @@
 </template>
 
 <script>
-  import { mapState } from 'vuex'
   export default {
-    computed: mapState([
-      'textos'
-    ]),
-    async asyncData ({ params }) {
+    data () {
       return {
-        slug: params.id
+        texto: this.$store.state.textos.find(x => x.slug === this.$route.params.id)
       }
     }
   }
