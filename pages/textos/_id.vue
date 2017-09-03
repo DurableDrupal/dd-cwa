e4186bc61905409e8f59419340d0c93e28b6e07c
Victor Kane <victorkane@gmail.com>
2017-09-03 10:09:17 -0300

    Use body-1 for body text, because body-1 puts 500 bold font weight for

--- pages/textos/_id.vue
+++ pages/textos/_id.vue
@@ -6,7 +6,7 @@
           <v-card-title class="headline mb-1">{{textos.find(x => x.slug === slug).titulo}}</v-card-title>
           <v-card-text>
             <p class="primary--text subheading mt-0">{{textos.find(x => x.slug === slug).autor}}</p>
-            <blockquote class="body-2" v-html="textos.find(x => x.slug === slug).texto"></blockquote>
+            <blockquote class="body-1" v-html="textos.find(x => x.slug === slug).texto"></blockquote>
             <p class="primary--text mt-2">{{textos.find(x => x.slug === slug).fecha}}</p>
           </v-card-text>
           <v-card-actions><p><nuxt-link to="/textos">Volver a la lista de textos</nuxt-link></p></v-card-actions>
