e3c2d0dd60c0a3841041fbe1a019ac1ddf6bfc5b
Rodolfo <rodolfo.guagnini@gmail.com>
2017-09-15 11:33:03 +0000

    finish translation of support.vue

--- pages/support.vue
+++ pages/support.vue
@@ -6,7 +6,11 @@
         <v-card-text>
           <p>Donate with <a href="https://www.paypal.com" target="_blank">PayPal</a> or <a href="https://blockchain.info" target="_blank">BitCoin</a></p>
           <p>Check our <a href="https://github.com/DurableDrupal/awebfactory-content-migration-rescue" target="_blank">GitHub Repo</a> and help us to reach 100 stars by <a href="#" target="_blank">giving us one</a></p>
-          <p>Comunicarse con nosotros a través de <v-btn fab small primary dark title="@durable_drupal" href="http://twitter.com/durable_drupal"><v-icon>fa-twitter</v-icon></v-btn> Twitter</p>
+          <p>Reach us at</p>
+          <p>
+            <v-btn fab small primary dark title="@durable_drupal" href="http://twitter.com/durable_drupal/"><v-icon>fa-twitter</v-icon></v-btn> Twitter
+            <v-btn fab small primary dark title="@DurableDrupal" href="https://www.facebook.com/DurableDrupal/"><v-icon>fa-facebook</v-icon></v-btn> Twitter
+          </p>
         </v-card-text>
       </v-card>
     </v-flex>
