14100820347e5cb0070509f3db6b4a46cb93a909
Rodolfo <rodolfo.guagnini@gmail.com>
2017-09-15 12:52:39 +0000

    changed buttons to flat as suggested in #1 but the problem persist, moving on for now

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,10 +4,11 @@
       <v-card>
         <v-card-title class="headline">Contact DurableDrupal</v-card-title>
         <v-card-text>
-          <p>Reach us at </p>
+          <p>Reach us at</p>
           <p>
-             <v-btn fab small primary dark title="@durable_drupal" href="http://twitter.com/durable_drupal/"><v-icon>fa-twitter</v-icon></v-btn> Twitter or 
-             <v-btn fab small primary dark title="@DurableDrupal" href="https://www.facebook.com/DurableDrupal/"><v-icon>fa-facebook</v-icon></v-btn> Facebook
+             <v-btn flat dark primary title="@durable_drupal" href="http://twitter.com/durable_drupal/">Twitter</v-btn>
+             <v-btn flat dark primary title="@durable_drupal" href="http://twitter.com/durable_drupal/">Twitter</v-btn> or 
+             <v-btn flat dark primary title="@DurableDrupal" href="https://www.facebook.com/DurableDrupal/">Facebook</v-btn>
           </p>
         </v-card-text>
       </v-card>
