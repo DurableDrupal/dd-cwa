d29a2319086a5208d4828d8edf36da33079a3308
Rodolfo <rodolfo.guagnini@gmail.com>
2017-09-15 14:30:28 +0000

    fixed leftovers of old text in index.vue, changed favicon.png file with durabledrupal branded one

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,8 @@
         <v-card-title class="headline">Welcome to DurableDrupal</v-card-title>
         <v-card-text>
           <p>DurableDrupal was created to explore alternatives to migrate from legacy
-             drupal 6,7 and even 8 sites to an API-first, full stack solution<a href="https://www.marxists.org/espanol/m-e/1840s/45-feuer.htm" title="Los filósofos no han hecho más que interpretar de diversos modo el mundo, pero de lo que se trata es de transformarlo." target="_blank">praxis</a> literaria.</p>
+             drupal 6,7 and even 8 sites to an API-first, full stack solution
+          </p>
           <div class="text-xs-right">
             <em><small>&mdash; Victor Kane</small></em>
           </div>
