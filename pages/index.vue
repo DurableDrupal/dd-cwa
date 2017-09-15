46642ceb6342a1ac0cda680bec685f61c0f823ee
Rodolfo <rodolfo.guagnini@gmail.com>
2017-09-15 13:12:56 +0000

    Scaffolded index.vue page, sample first paragraph added, lots of commented out code removed

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,15 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-    <!--
-      <div class="text-xs-center">
-        <img src="/logo.png" alt="PraxisPoetica" class="mb-5" />
-      </div>
-    -->
       <v-card>
-        <v-card-title class="headline">Bienvenidos al futuro hogar de la comunidad PraxisPoetica</v-card-title>
+        <v-card-title class="headline">Welcome to DurableDrupal</v-card-title>
         <v-card-text>
-          <p>PraxisPoetica existirá siempre para proveer recursos para la <a href="https://www.marxists.org/espanol/m-e/1840s/45-feuer.htm" title="Los filósofos no han hecho más que interpretar de diversos modo el mundo, pero de lo que se trata es de transformarlo." target="_blank">praxis</a> literaria.</p>
-          <p>En el contexto de la comunidad de cultura abierta en lucha para refundar la sociedad sobre nuevas bases</p>
-          <p>Sin la explotación del hombre por el hombre</p>
-          <p>Aquí se econtrarán talleres, textos, maneras de participar individualmente y también formando grupos de afinidad para trabajar y disfrutar en la praxis poética.</p>
+          <p>DurableDrupal was created to explore alternatives to migrate from legacy
+             drupal 6,7 and even 8 sites to an API-first, full stack solution<a href="https://www.marxists.org/espanol/m-e/1840s/45-feuer.htm" title="Los filósofos no han hecho más que interpretar de diversos modo el mundo, pero de lo que se trata es de transformarlo." target="_blank">praxis</a> literaria.</p>
           <div class="text-xs-right">
             <em><small>&mdash; Victor Kane</small></em>
           </div>
-          <!--
-          <hr class="my-3">
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br>
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank">Nuxt GitHub</a>
-          -->
         </v-card-text>
-          <!--
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn primary flat nuxt to="/inspire">Continue</v-btn>
-        </v-card-actions>
-          -->
       </v-card>
     </v-flex>
   </v-layout>
