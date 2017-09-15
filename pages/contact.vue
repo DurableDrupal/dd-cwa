734f642b42c5b28ffe8cf9f7cc4a0daf636899da
Rodolfo <rodolfo.guagnini@gmail.com>
2017-09-15 11:35:57 +0000

    changed content in contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,12 +2,13 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       <v-card>
-        <v-card-title class="headline">Reach durabledrupal</v-card-title>
+        <v-card-title class="headline">Contact DurableDrupal</v-card-title>
         <v-card-text>
           <p>Reach us at </p>
-          <p><v-btn hidden fab small primary dark title="@durable_drupal" href="http://twitter.com/durable_drupal/"><v-icon>fa-twitter</v-icon></v-btn>
+          <p>
              <v-btn fab small primary dark title="@durable_drupal" href="http://twitter.com/durable_drupal/"><v-icon>fa-twitter</v-icon></v-btn> Twitter or 
-             <v-btn fab small primary dark title="@DurableDrupal" href="https://www.facebook.com/DurableDrupal/"><v-icon>fa-facebook</v-icon></v-btn> Facebook</p>
+             <v-btn fab small primary dark title="@DurableDrupal" href="https://www.facebook.com/DurableDrupal/"><v-icon>fa-facebook</v-icon></v-btn> Facebook
+          </p>
         </v-card-text>
       </v-card>
     </v-flex>
