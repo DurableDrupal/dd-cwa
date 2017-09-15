ba72a245fbf793efc253004a22da7b0814f19b96
Rodolfo <rodolfo.guagnini@gmail.com>
2017-09-15 11:00:35 +0000

    update contacto.vue page, workaround a icon-desapearing bug at refresh, fixes #1

--- pages/contacto.vue
+++ pages/contacto.vue
@@ -2,9 +2,12 @@
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
       <v-card>
-        <v-card-title class="headline">Comunicarse con PraxisPoetica</v-card-title>
+        <v-card-title class="headline">Reach durabledrupal</v-card-title>
         <v-card-text>
-          <p>Comunicarse con nosotros a través de <v-btn fab small primary dark title="@praxispoetica" href="http://twitter.com/praxispoetica"><v-icon>fa-twitter</v-icon></v-btn> Twitter</p>
+          <p>Reach us at </p>
+          <p><v-btn hidden fab small primary dark title="@durable_drupal" href="http://twitter.com/durable_drupal/"><v-icon>fa-twitter</v-icon></v-btn>
+             <v-btn fab small primary dark title="@durable_drupal" href="http://twitter.com/durable_drupal/"><v-icon>fa-twitter</v-icon></v-btn> Twitter or 
+             <v-btn fab small primary dark title="@DurableDrupal" href="https://www.facebook.com/DurableDrupal/"><v-icon>fa-facebook</v-icon></v-btn> Facebook</p>
         </v-card-text>
       </v-card>
     </v-flex>
