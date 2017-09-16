5a035c15e000cdefdd0ed8912203ca9398ca2b50
Rodolfo <rodolfo.guagnini@gmail.com>
2017-09-16 00:13:48 +0000

    restore Roboto as default font style

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ module.exports = {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'shortcut icon', type: 'image/png', href: '/favicon.png' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }, 
       { rel: 'stylesheet', href: '/css/font-awesome.min.css' }
     ]
   },
