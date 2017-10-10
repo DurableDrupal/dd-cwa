9e210b0b26eb2ab36d81277a5c490197ba97dc38
victorkane <victorkane@gmail.com>
2017-10-10 10:07:49 -0400

    styling

--- components/feature-card.vue
+++ components/feature-card.vue
@@ -1,13 +1,12 @@
 <template>
-  <v-card class="ma-1">
+  <v-card>
     <v-card-title primary-title> 
       <div>
-        <h3 class="text-title">{{ text.metaData.itemName }}</h3>
+        <h5 class="text-title">{{ text.metaData.itemName }}</h5>
       </div>
     </v-card-title>
     <v-card-text>
       <div class="body-1" v-html="marked(text.textBody.value)"></div>
-      <p class="primary--text mt-2">{{ text.metaData.publishedDate | dateEn }}</p>
     </v-card-text>
     <v-card-actions><p><nuxt-link to="/">Learn more...</nuxt-link></p></v-card-actions>
   </v-card>
