4d2f9826716ae79ac713b3622af6a47c293a3de9
victorkane <victorkane@gmail.com>
2017-10-10 12:11:45 -0400

    content arrangements for home page feature cards

--- components/feature-card.vue
+++ components/feature-card.vue
@@ -1,14 +1,19 @@
 <template>
   <v-card>
-    <v-card-title primary-title> 
+    <v-card-title class="pl-2 pr-2 pt-2 pb-0"> 
       <div>
         <h5 class="text-title">{{ text.metaData.itemName }}</h5>
       </div>
     </v-card-title>
-    <v-card-text>
-      <div class="body-1" v-html="marked(text.textBody.value)"></div>
+    <v-card-text class="ma-0 pa-0">
+      <!-- <div class="body-1" v-html="marked(text.textBody.value)"></div> -->
+      <div class="action-item" v-for="action in text.actions">
+        <v-btn class="ma-0 pa-0" flat block :to="action.to">
+          <div> {{ action.label }} </div>
+        </v-btn>
+      </div>
     </v-card-text>
-    <v-card-actions><p><nuxt-link to="/">Learn more...</nuxt-link></p></v-card-actions>
+
   </v-card>
 </template>
 
@@ -21,3 +26,10 @@
     props: ['text']
   }
 </script>
+
+<style>
+  .application--light .btn,
+  .application--light .btn .icon {
+    color: #01579b!important;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,18 @@
 <template>
  <v-container>
   <v-layout row wrap>
+    <v-flex xs12 mb-2>
+      <v-card>
+          <v-card-title primary-title> 
+            <div>
+              <h4>Join the DurableDrupal Content Migration Rescue Community</h4>
+            </div>
+          </v-card-title> 
+        </v-card> 
+      </v-card>
+    </v-flex>
+  </v-layout>
+  <v-layout row wrap>
     <v-flex xs12 sm4 mb-1>
       <FeatureCard :text="featureCommunity"></FeatureCard>
     </v-flex>
@@ -26,10 +38,26 @@
       let featureCommunity = await axios.get(process.env.scsurl + '/api/textsbyslug/community-20171009')
       let featureMentoring = await axios.get(process.env.scsurl + '/api/textsbyslug/mentoring-20171009')
       let featureRaas = await axios.get(process.env.scsurl + '/api/textsbyslug/raas-20171009')
+
+      let fc = featureCommunity.data
+      let fcactions = []
+      fcactions.push({icon: 'book', label: 'read the book', to: '/read-the-book'})
+      fcactions.push({icon: 'code', label: 'grab the code', to: '/grab-the-code'})
+      fcactions.push({icon: 'file-text', label: 'see case studies', to: '/case-studies'})
+      fcactions.push({icon: 'map', label: 'manage your projects', to: '/manage-projects'})
+      fc.actions = fcactions
+      let fm = featureMentoring.data
+      let fmactions = []
+      fmactions.push({icon: 'book', label: 'read the book', to: '/read-the-book'})
+      fm.actions = fmactions
+      let fr = featureRaas.data
+      let fractions = []
+      fractions.push({icon: 'book', label: 'read the book', to: '/read-the-book'})
+      fr.actions = fractions
       return {
-        featureCommunity: featureCommunity.data,
-        featureMentoring: featureMentoring.data,
-        featureRaas: featureRaas.data
+        featureCommunity: fc,
+        featureMentoring: fm,
+        featureRaas: fr
       }
     }
   }
